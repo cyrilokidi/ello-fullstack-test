e8c3592ee3788e245f1032933725f2cbe5f392f4
CyrilOkidi <okidicyril@gmail.com>
2024-06-11 09:34:16 +0300

    Create autocomplete

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Autocomplete, Box, Container, TextField } from "@mui/material";
 
 export default function HomePage() {
-    return <React.Fragment>
-        This is the home page.
-    </React.Fragment>
+    return <Container component="main">
+        <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <Box>
+                <Autocomplete
+                    sx={{ width: 500 }}
+                    options={[]}
+                    renderInput={(params) => <TextField
+                        {...params}
+                        placeholder="Search..."
+                    />}
+                />
+            </Box>
+        </Box>
+    </Container>
 }
