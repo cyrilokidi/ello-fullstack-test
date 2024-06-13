968dfe69080980aee6c741a7298af2969c91deca
CyrilOkidi <okidicyril@gmail.com>
2024-06-13 18:39:22 +0300

    Create document page

---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Main, Head, NextScript } from 'next/document'
+
+export default function Document(): JSX.Element {
+    return (
+        <Html>
+            <Head />
+
+            <body>
+                <Main />
+
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
