9ee13cbb95365d292b20451c48c935fa83e048c3
CyrilOkidi <okidicyril@gmail.com>
2024-06-14 00:05:13 +0300

    Create error snackbar

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,11 @@
-import { Box, Grid, Toolbar } from "@mui/material";
+import { Alert, Box, Grid, Snackbar, Toolbar } from "@mui/material";
 import React from "react";
 import { IBook } from "../data";
 import { useApp } from "../contexts/AppContext";
 import { BookCard } from "../components/BookCard";
 import { SearchInput } from "../components/SearchInput";
 import { NavigationMenu } from "../components/NavigationMenu";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, ApolloError } from "@apollo/client";
 
 const GET_BOOKS = gql`
   query GetBooks {
@@ -19,6 +19,7 @@ const GET_BOOKS = gql`
 `;
 
 export interface IUseFetchAllBooks {
+    error: any;
     books?: IBook[];
 }
 
@@ -26,6 +27,7 @@ export const useFetchAllBooks = (): IUseFetchAllBooks => {
     const { loading, error, data } = useQuery(GET_BOOKS);
 
     return {
+        error,
         books: data?.books,
     }
 };
@@ -35,44 +37,58 @@ export default function HomePage() {
 
     const app = useApp();
 
-    return <Box display="flex">
-        <NavigationMenu selectedBooksCount={app.selectedBooks.length} />
+    return <React.Fragment>
+        <Box display="flex">
+            <NavigationMenu selectedBooksCount={app.selectedBooks.length} />
 
-        <Box
-            component="main"
-            sx={{
-                p: 3,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}
-            gap={2}
-        >
-            <Toolbar />
-            <Box>
-                <SearchInput
-                    books={fetchAllBooks.books}
-                />
-            </Box>
+            <Box
+                component="main"
+                sx={{
+                    p: 3,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+                gap={2}
+            >
+                <Toolbar />
+                <Box>
+                    <SearchInput
+                        books={fetchAllBooks.books}
+                    />
+                </Box>
 
-            <Box>
-                <Grid container spacing={2}>
-                    {fetchAllBooks.books?.map((book, index) => <Grid
-                        key={index}
-                        item
-                        lg={2}
-                        md={3}
-                        xs={6}
-                    >
-                        <BookCard
-                            book={book}
-                            selectedBooks={app.selectedBooks}
-                            onAddBook={app.addBook}
-                            onRemoveBook={app.removeBook}
-                        />
-                    </Grid>)}
-                </Grid>
+                <Box>
+                    <Grid container spacing={2}>
+                        {fetchAllBooks.books?.map((book, index) => <Grid
+                            key={index}
+                            item
+                            lg={2}
+                            md={3}
+                            xs={6}
+                        >
+                            <BookCard
+                                book={book}
+                                selectedBooks={app.selectedBooks}
+                                onAddBook={app.addBook}
+                                onRemoveBook={app.removeBook}
+                            />
+                        </Grid>)}
+                    </Grid>
+                </Box>
             </Box>
         </Box>
-    </Box>
+
+        <Snackbar
+            open={fetchAllBooks.error instanceof ApolloError}
+            autoHideDuration={6000}
+        >
+            <Alert
+                severity="error"
+                sx={{ width: '100%' }}
+            >
+                {fetchAllBooks.error?.message as string}
+            </Alert>
+        </Snackbar>
+    </React.Fragment>
 }
