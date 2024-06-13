d2bfe068eaeb530129f29c538c9a59cb11b6587a
CyrilOkidi <okidicyril@gmail.com>
2024-06-13 08:00:52 +0300

    Create search input component

---
+++ frontend/components/SearchInput.tsx
@@ -0,0 +1,33 @@
+import { Autocomplete, Box, TextField, Typography } from "@mui/material"
+import { IBook } from "../data"
+
+export interface ISearchInputProps {
+    books: IBook[];
+}
+
+export const SearchInput = (props: ISearchInputProps): JSX.Element => {
+    return <Autocomplete
+        sx={{ width: 500 }}
+        options={props.books}
+        getOptionLabel={(book) => book.title}
+        renderOption={(props, book) => <Box
+            {...props}
+            key={book.title + book.author + book.readingLevel}
+            component="li"
+            sx={{
+                '& > img': {
+                    mr: 2,
+                    flexShrink: 0,
+                }
+            }}
+        >
+            <Typography variant="body1" color="text.primary">
+                {book.title}
+            </Typography>
+        </Box>}
+        renderInput={(params) => <TextField
+            {...params}
+            placeholder="Search..."
+        />}
+    />
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,9 @@
-import { Autocomplete, Box, Container, Grid, TextField, Typography } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 import React from "react";
 import { IBook, demoBooks } from "../data";
 import { useApp } from "../contexts/AppContext";
 import { BookCard } from "../components/BookCard";
+import { SearchInput } from "../components/SearchInput";
 
 export interface IUseFetchAllBooks {
     books: IBook[];
@@ -39,29 +40,8 @@ export default function HomePage() {
             gap={2}
         >
             <Box>
-                <Autocomplete
-                    sx={{ width: 500 }}
-                    options={fetchAllBooks.books}
-                    getOptionLabel={(book) => book.title}
-                    renderOption={(props, book) => <Box
-                        {...props}
-                        key={book.title + book.author + book.readingLevel}
-                        component="li"
-                        sx={{
-                            '& > img': {
-                                mr: 2,
-                                flexShrink: 0,
-                            }
-                        }}
-                    >
-                        <Typography variant="body1" color="text.primary">
-                            {book.title}
-                        </Typography>
-                    </Box>}
-                    renderInput={(params) => <TextField
-                        {...params}
-                        placeholder="Search..."
-                    />}
+                <SearchInput
+                    books={fetchAllBooks.books}
                 />
             </Box>
 
