7047444ace40647136fa88f493099687d781c2f8
CyrilOkidi <okidicyril@gmail.com>
2024-06-13 14:28:14 +0300

    Add app bar

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,12 @@
-import { Box, Container, Grid } from "@mui/material";
+import { AppBar, Box, Button, Container, Divider, Drawer, Grid, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import { IBook, demoBooks } from "../data";
 import { useApp } from "../contexts/AppContext";
 import { BookCard } from "../components/BookCard";
 import { SearchInput } from "../components/SearchInput";
+import {
+    Menu as MenuIcon,
+} from "@mui/icons-material";
 
 export interface IUseFetchAllBooks {
     books: IBook[];
@@ -24,21 +27,96 @@ export const useFetchAllBooks = (): IUseFetchAllBooks => {
     }
 };
 
+const drawerWidth = 240;
+
 export default function HomePage() {
     const fetchAllBooks = useFetchAllBooks();
 
     const app = useApp();
 
-    return <Container component="main">
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                Ello
+            </Typography>
+
+            <Divider />
+
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton sx={{ textAlign: 'center' }}>
+                        <ListItemText primary="Reading List" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
+
+    return <Box display="flex">
+        <AppBar component="nav">
+            <Toolbar>
+                {/* <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    edge="start"
+                    onClick={handleDrawerToggle}
+                    sx={{ mr: 2, display: { sm: 'none' } }}
+                >
+                    <MenuIcon />
+                </IconButton> */}
+
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                >
+                    Ello
+                </Typography>
+
+                <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+
+                    <Button sx={{ color: '#fff' }}>
+                        Reading List
+                    </Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
+
+        <nav>
+            <Drawer
+                // container={container}
+                variant="temporary"
+                open={mobileOpen}
+                onClose={handleDrawerToggle}
+                ModalProps={{
+                    keepMounted: true, // Better open performance on mobile.
+                }}
+                sx={{
+                    display: { xs: 'block', sm: 'none' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                }}
+            >
+                {drawer}
+            </Drawer>
+        </nav>
+
         <Box
+            component="main"
             sx={{
-                marginTop: 8,
+                p: 3,
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
             }}
             gap={2}
         >
+            <Toolbar />
             <Box>
                 <SearchInput
                     books={fetchAllBooks.books}
@@ -50,7 +128,9 @@ export default function HomePage() {
                     {fetchAllBooks.books.map((book, index) => <Grid
                         key={index}
                         item
-                        xs={2}
+                        lg={2}
+                        md={3}
+                        xs={6}
                     >
                         <BookCard
                             book={book}
@@ -62,5 +142,40 @@ export default function HomePage() {
                 </Grid>
             </Box>
         </Box>
-    </Container>
+    </Box>
+
+    // return <Container component="main">
+    //     <Box
+    //         sx={{
+    //             marginTop: 8,
+    //             display: 'flex',
+    //             flexDirection: 'column',
+    //             alignItems: 'center',
+    //         }}
+    //         gap={2}
+    //     >
+    //         <Box>
+    //             <SearchInput
+    //                 books={fetchAllBooks.books}
+    //             />
+    //         </Box>
+
+    //         <Box>
+    //             <Grid container spacing={2}>
+    //                 {fetchAllBooks.books.map((book, index) => <Grid
+    //                     key={index}
+    //                     item
+    //                     xs={2}
+    //                 >
+    //                     <BookCard
+    //                         book={book}
+    //                         selectedBooks={app.selectedBooks}
+    //                         onAddBook={app.addBook}
+    //                         onRemoveBook={app.removeBook}
+    //                     />
+    //                 </Grid>)}
+    //             </Grid>
+    //         </Box>
+    //     </Box>
+    // </Container>
 }
