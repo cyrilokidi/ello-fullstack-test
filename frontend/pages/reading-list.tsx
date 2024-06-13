3f127eee1443e591b9b16088beeb8b37ab348353
CyrilOkidi <okidicyril@gmail.com>
2024-06-13 19:17:23 +0300

    Conditionally render reading list books

--- frontend/pages/reading-list.tsx
+++ frontend/pages/reading-list.tsx
@@ -1,11 +1,14 @@
-import { Box, Grid, Toolbar } from "@mui/material";
+import { Box, Button, Container, Grid, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import { useApp } from "../contexts/AppContext";
 import { BookCard } from "../components/BookCard";
 import { NavigationMenu } from "../components/NavigationMenu";
+import { useRouter } from "next/router";
 
 export default function HomePage() {
     const app = useApp();
+
+    const router = useRouter();
 
     return <Box display="flex">
         <NavigationMenu selectedBooksCount={app.selectedBooks.length} />
@@ -24,20 +27,45 @@ export default function HomePage() {
 
             <Box>
                 <Grid container spacing={2}>
-                    {app.selectedBooks.map((book, index) => <Grid
-                        key={index}
-                        item
-                        lg={2}
-                        md={3}
-                        xs={6}
-                    >
-                        <BookCard
-                            book={book}
-                            selectedBooks={app.selectedBooks}
-                            onAddBook={app.addBook}
-                            onRemoveBook={app.removeBook}
-                        />
-                    </Grid>)}
+                    {(!app.selectedBooks.length)
+                        ? <Box component="main">
+                            <Box
+                                gap={1}
+                                sx={{
+                                    p: 4,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                }}
+                            >
+                                <Typography>
+                                    No book(s) added
+                                </Typography>
+
+                                <Button
+                                    variant="contained"
+                                    onClick={() => router.push('/')}
+                                    fullWidth
+                                    color="secondary"
+                                >
+                                    add
+                                </Button>
+                            </Box>
+                        </Box>
+                        : app.selectedBooks.map((book, index) => <Grid
+                            key={index}
+                            item
+                            lg={2}
+                            md={3}
+                            xs={6}
+                        >
+                            <BookCard
+                                book={book}
+                                selectedBooks={app.selectedBooks}
+                                onAddBook={app.addBook}
+                                onRemoveBook={app.removeBook}
+                            />
+                        </Grid>)}
                 </Grid>
             </Box>
         </Box>
